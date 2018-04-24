ad10ed4248491e7d82556aaea6bdfeaac104e77f
Kevin Ma <ma65536@gmail.com>
2018-04-24 16:36:48 -0400

    Create a functional components and inject into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import User from './User/User';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React JSX</h1>  
+        <User />
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'React JSX'));
---
+++ src/User/User.js
@@ -0,0 +1,10 @@
+// create a funcaitonal based component and then update it in app.js
+// function name should be camelCase
+
+import React from 'react';
+
+const user = () => {
+    return <p>This is create a customized component</p>
+};
+
+export default user;
