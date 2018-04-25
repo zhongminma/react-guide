5511ff24f8d5a230310e4ed9720c229dcb05453e
Kevin Ma <ma65536@gmail.com>
2018-04-25 01:50:54 -0400

    Use setState in eventHandler to re-render DOM

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 import User from './User/User';
 
 class App extends Component {
+  state = {
+    users: [
+      { name: 'Dan', gender: 'male'},
+      { name: 'Rachel', gender: 'female'}
+    ]
+  }
+
+  checkFullNamehandler = () => {
+    //console.log('button clicked');
+    //this.state.users[0].name = 'Danial Ray';
+    this.setState({users:[
+      { name: 'Daniel K', gender: 'male'},
+      { name: 'Rachel M', gender: 'female'},
+      { name: 'Bill J', gender: 'male'},
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React JSX</h1>  
-        <User name='Dan' gender='male'/>
-        <User name='Rachel' gender='female'>Email: rachel@gmail.com</User>
+        <User name={this.state.users[0].name} gender={this.state.users[0].gender}/>
+        <User name={this.state.users[1].name} gender={this.state.users[1].gender}>
+          Email: rachel@gmail.com
+        </User>
+        <button onClick={this.checkFullNamehandler}>Check Full Name</button>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'React JSX'));
